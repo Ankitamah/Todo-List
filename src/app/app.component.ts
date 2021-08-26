5919b3db43cff54d623b60614021b65249e77988
Ankita Maheshwari <amaheshwari@splunk.com>
2021-08-26 22:15:18 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from "./todo";
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
-}
+  title = "Todo List";
+
+  todoValue!: string;
+  list: Todo[] = [];
+
+  ngOninit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem(){
+    if(this.todoValue !== ""){
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+  deleteItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
+    
+  }
